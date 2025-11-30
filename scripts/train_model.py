b49e357f285ba926f611912815a4a769a7048812
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-11-30 18:30:47 +0530

    Update train_model.py

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("../datasets/dataset1.csv")
+
+model = LinearRegression()
+model.fit(df[['price']], df['sales'])
+
+print("Model trained successfully!")
+print("Coefficient:", model.coef_[0])
 
