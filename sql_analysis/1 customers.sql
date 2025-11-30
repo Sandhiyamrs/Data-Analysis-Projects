c55781e56480db733b04ea52ff297fc2643cdbdd
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-11-30 18:43:35 +0530

    Create 1 customers.sql

---
+++ sql_analysis/1 customers.sql
@@ -0,0 +1,12 @@
+CREATE TABLE customers (
+    id INTEGER,
+    name TEXT,
+    age INTEGER,
+    spend INTEGER
+);
+
+INSERT INTO customers VALUES
+(1,'Asha',23,1200),
+(2,'Rahul',35,540),
+(3,'Divya',29,680),
+(4,'Karan',41,1100);
