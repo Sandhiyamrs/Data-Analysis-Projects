4b941a2d6cf8b7a90d59299cbe605a519a161ae4
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-11-30 18:24:00 +0530

    Update clean_data.py

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -1,1 +1,9 @@
+import pandas as pd
+
+def clean_dataset(path):
+    df = pd.read_csv(path)
+    df = df.drop_duplicates()
+    df = df.fillna(0)
+    df.to_csv(path, index=False)
+    print("Cleaned:", path)
 
