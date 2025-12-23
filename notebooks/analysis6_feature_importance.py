6f21b05bd2ea773bf1cdb2b2ac75a9bb2b403e43
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-23 21:06:47 +0530

    Create analysis6_feature_importance.py

---
+++ notebooks/analysis6_feature_importance.py
@@ -0,0 +1,8 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+
+X, y = load_iris(return_X_y=True)
+model = RandomForestClassifier()
+model.fit(X, y)
+
+print(model.feature_importances_)
