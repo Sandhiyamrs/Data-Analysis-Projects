617d507227f487dfb67b38b60b0659d101ddfda5
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-23 20:57:01 +0530

    Create 6_lightgbm_classifier.py

---
+++ advanced_ml/6_lightgbm_classifier.py
@@ -0,0 +1,11 @@
+import lightgbm as lgb
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = lgb.LGBMClassifier()
+model.fit(X_train, y_train)
+
+print("Accuracy:", model.score(X_test, y_test))
