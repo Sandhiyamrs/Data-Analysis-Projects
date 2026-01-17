27b00f94d9f5b09d7f5b06876e1aecf2a06cfaff
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-17 21:01:29 +0530

    Update 6_lightgbm_classifier.py

--- advanced_ml/6_lightgbm_classifier.py
+++ advanced_ml/6_lightgbm_classifier.py
@@ -1,11 +1,21 @@
 import lightgbm as lgb
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-X, y = load_iris(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X, y = load_breast_cancer(return_X_y=True)
 
-model = lgb.LGBMClassifier()
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = lgb.LGBMClassifier(
+    n_estimators=300,
+    learning_rate=0.05,
+    max_depth=6
+)
+
 model.fit(X_train, y_train)
+preds = model.predict(X_test)
 
-print("Accuracy:", model.score(X_test, y_test))
+print("Accuracy:", accuracy_score(y_test, preds))
