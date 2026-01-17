2452705c56c94634d0957d85de71cc50263a0472
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-17 21:03:46 +0530

    Update 9_bayesian_optimization.py

--- advanced_ml/9_bayesian_optimization.py
+++ advanced_ml/9_bayesian_optimization.py
@@ -1,17 +1,23 @@
-from skopt import BayesSearchCV
+from bayes_opt import BayesianOptimization
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import load_iris
 
-X, y = load_iris(return_X_y=True)
+X, y = load_breast_cancer(return_X_y=True)
 
-search = BayesSearchCV(
-    RandomForestClassifier(),
-    {
-        'n_estimators': (50, 300),
-        'max_depth': (3, 20)
-    },
-    n_iter=10
+def rf_cv(n_estimators, max_depth):
+    model = RandomForestClassifier(
+        n_estimators=int(n_estimators),
+        max_depth=int(max_depth),
+        random_state=42
+    )
+    return cross_val_score(model, X, y, cv=5).mean()
+
+optimizer = BayesianOptimization(
+    f=rf_cv,
+    pbounds={"n_estimators": (50, 300), "max_depth": (3, 15)},
+    random_state=42
 )
 
-search.fit(X, y)
-print("Best Params:", search.best_params_)
+optimizer.maximize(init_points=5, n_iter=10)
+print(optimizer.max)
