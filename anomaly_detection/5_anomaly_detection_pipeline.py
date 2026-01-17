d4eace17277f66b78290b6938d59dd5af7882912
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-17 21:06:04 +0530

    Update 5_anomaly_detection_pipeline.py

--- anomaly_detection/5_anomaly_detection_pipeline.py
+++ anomaly_detection/5_anomaly_detection_pipeline.py
@@ -1,9 +1,26 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import IsolationForest
-import numpy as np
 
-data = np.array([[10],[12],[11],[13],[100]])
-model = IsolationForest(contamination=0.2)
-labels = model.fit_predict(data)
+class AnomalyPipeline:
+    def __init__(self):
+        self.scaler = StandardScaler()
+        self.model = IsolationForest(
+            contamination=0.05,
+            random_state=42
+        )
 
-anomalies = data[labels == -1]
-print("Detected anomalies:", anomalies)
+    def fit(self, X):
+        X_scaled = self.scaler.fit_transform(X)
+        self.model.fit(X_scaled)
+
+    def predict(self, X):
+        X_scaled = self.scaler.transform(X)
+        return self.model.predict(X_scaled)
+
+if __name__ == "__main__":
+    df = pd.read_csv("dataset1.csv")
+    pipeline = AnomalyPipeline()
+    pipeline.fit(df)
+    df["anomaly"] = pipeline.predict(df)
+    print(df[df["anomaly"] == -1])
