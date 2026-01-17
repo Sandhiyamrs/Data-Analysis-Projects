40808a960c9122761cc79f821b85698eac99d205
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-17 21:04:15 +0530

    Update 10_model_explainability_shap.py

--- advanced_ml/10_model_explainability_shap.py
+++ advanced_ml/10_model_explainability_shap.py
@@ -1,12 +1,18 @@
 import shap
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import load_iris
+import xgboost as xgb
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
 
-X, y = load_iris(return_X_y=True)
-model = RandomForestClassifier()
-model.fit(X, y)
+X, y = load_breast_cancer(return_X_y=True, as_frame=True)
 
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(X)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-print("SHAP values calculated")
+model = xgb.XGBClassifier(eval_metric="logloss")
+model.fit(X_train, y_train)
+
+explainer = shap.Explainer(model)
+shap_values = explainer(X_test)
+
+shap.summary_plot(shap_values, X_test)
